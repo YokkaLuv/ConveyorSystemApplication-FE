280d971574e94085cf66d0eac4b79e528c5e6d18
Yolk <100048542+YokkaLuv@users.noreply.github.com>
2025-04-26 01:22:53 +0700

    Rename .java to .kt

--- app/src/main/java/com/doan/mechacal/MainActivity.kt
+++ app/src/main/java/com/doan/mechacal/MainActivity.kt
@@ -1,23 +1,24 @@
-package com.doan.mechacal
+package com.doan.mechacal;
 
-import android.content.Intent
-import android.os.Bundle
-import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
-import com.doan.mechacal.CreateSessionActivity
-import com.doan.mechacal.UserFrontpage
-import com.doan.mechacal.R
+import android.os.Bundle;
 
-class MainActivity : AppCompatActivity() {
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
 
-        val btnSignin: Button = findViewById(R.id.btn_signin)
-        btnSignin.setOnClickListener {
-            val intent = Intent(this, UserFrontpage::class.java)
-            startActivity(intent)
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }

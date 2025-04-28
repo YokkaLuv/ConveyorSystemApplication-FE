9f14b7524fb7b70e8808ff7afeec948a8a38825e
Quanh_2211 <153338584+QuanhBK@users.noreply.github.com>
2025-04-28 12:06:18 +0700

    Update Chapter2resultActivity.kt

--- app/src/main/java/com/doan/mechacal/Chapter2resultActivity.kt
+++ app/src/main/java/com/doan/mechacal/Chapter2resultActivity.kt
@@ -28,23 +28,17 @@ class Chapter2resultActivity : AppCompatActivity() {
         val force = intent.getDoubleExtra("F", 0.0)
         val nx = intent.getDoubleExtra("n_x", 0.0)
         val d1 = intent.getDoubleExtra("d1", 0.0)
-        val z1 = intent.getIntExtra("z1", 0)
-        val z2 = intent.getIntExtra("z2", 0)
         val ux = intent.getDoubleExtra("u_x", 0.0)
 
         // Hiển thị kết quả
         val resultForce = findViewById<TextView>(R.id.resultForce)
         val resultNx = findViewById<TextView>(R.id.resultNx)
         val resultD1 = findViewById<TextView>(R.id.resultD1)
-        val resultZ1 = findViewById<TextView>(R.id.resultZ1)
-        val resultZ2 = findViewById<TextView>(R.id.resultZ2)
         val resultUx = findViewById<TextView>(R.id.resultUx)
 
         resultForce.text = "$force"
         resultNx.text = "$nx"
         resultD1.text = "$d1"
-        resultZ1.text = "$z1 răng"
-        resultZ2.text = "$z2 răng"
         resultUx.text = "$ux"
     }
 }
